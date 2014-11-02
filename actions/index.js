045932fd19be6526a439ea0c630e0baa2d2ee1c2
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2014-11-01 22:21:05 -0600

    Get convert action and tests

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,7 @@
-var nconf = require('nconf'),
+var doc = require('../lib'),
+    exec = require('child_process').exec,
+    fse = require('fs-extra'),
+    nconf = require('nconf'),
     q = require('q'),
     winston = require('winston');
 
@@ -7,18 +10,44 @@ module.exports = function() {
     // Logging stuff           
     nconf.file({ file: './gebo.json' });
     var logLevel = nconf.get('logLevel'),
-        timeout = nconf.get('pdf2htmlEX').timeout;
-    var logger = new (winston.Logger)({ transports: [ new (winston.transports.Console)({ colorize: true }) ] });
+        logger = new (winston.Logger)({ transports: [ new (winston.transports.Console)({ colorize: true }) ] });
+
+    // For producing the download URL
+    var domain = nconf.get('domain');
 
     /**
-     * The following provides an example of the basic pattern of
-     * every gebo action
+     * Convert the given PDF to HTML
+     * 
+     * @param object 
+     * @param object 
+     * 
+     * @return promise 
      */
     exports.convert = function(verified, message) {
         var deferred = q.defer();
     
         if (verified.admin || verified.execute) {
-          deferred.resolve();
+          var destDir = './public/' + message.file.path.split('/').pop();
+
+          // Respect the original file name
+          var filename = doc.getOutputFileName(message.file.originalname, 'html');
+
+          doc.convert(message.file.path, destDir, filename, function(err, path) {
+                if (err) {
+                  if (logLevel === 'trace') logger.error('gebo-pdf2htmlEX:', err);
+                  deferred.resolve({ error: err });
+                }
+                else {
+                  if (message.content && message.content.raw) {
+                    fse.realpath(destDir, function(err, resolvedPath) {
+                        deferred.resolve({ filePath: resolvedPath + '/' + filename, fileName: filename });
+                      });
+                  }
+                  else {
+                    deferred.resolve(domain + '/' + message.file.path.split('/').pop() + '/' + filename);
+                  }
+                }
+            });
         }
         else {
           deferred.reject('You are not permitted to request or propose that action');
--- test/index.js
+++ test/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var actionModule = require('..');
+var actionModule = require('..'),
+    fse = require('fs-extra'),
+    nconf = require('nconf');
+
+var DOMAIN = nconf.get('domain');
 
 /**
  * convert
@@ -8,17 +12,19 @@ var actionModule = require('..');
 exports.convert = {
 
     setUp: function(callback) {
+        fse.createReadStream('./test/docs/pdf.pdf').pipe(fse.createWriteStream('/tmp/pdf.pdf'));
         callback();
     },
 
     tearDown: function(callback) {
+        fse.unlinkSync('/tmp/pdf.pdf');
         callback();
     },
 
     'Reject an agent with inadequate permission': function(test) {
         test.expect(1);
         actionModule.actions.convert({ resource: 'convert' }, {}).
-            then(function(greeting) {
+            then(function(page) {
                 test.ok(false, 'Shouldn\'t get here');
                 test.done();
               }).
@@ -28,11 +34,28 @@ exports.convert = {
               });
     },
 
-    'Return a greeting to an agent with execute permission': function(test) {
-        test.expect(1);
-        actionModule.actions.convert({ resource: 'convert', execute: true }, {}).
-            then(function(greeting) {
-                test.ok(true);
+    'Convert PDF to HTML and return raw data': function(test) {
+        test.expect(3);
+        actionModule.actions.convert({ resource: 'convert', execute: true },
+                                     { content: { raw: true },
+                                       file: {
+                                            path: '/tmp/pdf.pdf',
+                                            originalname: 'my.pdf',
+                                            type: 'application/pdf',
+                                            size: 19037,
+                                       },
+              }).
+            then(function(page) {
+                test.equal(page.filePath, fse.realpathSync('./public/pdf.pdf/my.html'));
+                test.equal(page.fileName, 'my.html');
+                try {
+                  fse.closeSync(fse.openSync(page.filePath, 'r'));
+                  fse.removeSync('./public/pdf.pdf');
+                  test.ok(true);
+                }
+                catch (err) {
+                  test.ok(false, err);
+                }
                 test.done();
               }).
             catch(function(err) {
@@ -41,5 +64,30 @@ exports.convert = {
               });
     },
 
-
+    'Convert PDF to HTML and return a link': function(test) {
+        test.expect(2);
+        actionModule.actions.convert({ resource: 'convert', execute: true },
+                                     { file: {
+                                            path: '/tmp/pdf.pdf',
+                                            originalname: 'my.pdf',
+                                            type: 'application/pdf',
+                                            size: 19037,
+                                       },
+              }).
+            then(function(link) {
+                test.equal(link, DOMAIN + '/pdf.pdf/my.html');
+                try {
+                  fse.removeSync('./public/pdf.pdf');
+                  test.ok(true);
+                }
+                catch (err) {
+                  test.ok(false, err);
+                }
+                test.done();
+              }).
+            catch(function(err) {
+                test.ok(false, err);      
+                test.done();
+              });
+    },
 };
